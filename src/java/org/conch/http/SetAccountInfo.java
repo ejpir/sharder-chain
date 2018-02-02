5d289c1d5d51781a778ec8ba428335d08018590b
Aron.j <xy@ichaoj.com>
2018-02-03 01:14:19 +0800

    account

---
+++ src/java/org/conch/http/SetAccountInfo.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright © 2017 sharder.org.
+ * Copyright © 2014-2017 ichaoj.com.
+ *
+ * See the LICENSE.txt file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with ichaoj.com,
+ * no part of the COS software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE.txt file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+
+package org.conch.http;
+
+import org.conch.Account;
+import org.conch.Attachment;
+import org.conch.Constants;
+import org.conch.ConchException;
+import org.conch.util.Convert;
+import org.json.simple.JSONStreamAware;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.conch.http.JSONResponses.INCORRECT_ACCOUNT_DESCRIPTION_LENGTH;
+import static org.conch.http.JSONResponses.INCORRECT_ACCOUNT_NAME_LENGTH;
+
+public final class SetAccountInfo extends CreateTransaction {
+
+    static final SetAccountInfo instance = new SetAccountInfo();
+
+    private SetAccountInfo() {
+        super(new APITag[] {APITag.ACCOUNTS, APITag.CREATE_TRANSACTION}, "name", "description");
+    }
+
+    @Override
+    protected JSONStreamAware processRequest(HttpServletRequest req) throws ConchException {
+
+        String name = Convert.nullToEmpty(req.getParameter("name")).trim();
+        String description = Convert.nullToEmpty(req.getParameter("description")).trim();
+
+        if (name.length() > Constants.MAX_ACCOUNT_NAME_LENGTH) {
+            return INCORRECT_ACCOUNT_NAME_LENGTH;
+        }
+
+        if (description.length() > Constants.MAX_ACCOUNT_DESCRIPTION_LENGTH) {
+            return INCORRECT_ACCOUNT_DESCRIPTION_LENGTH;
+        }
+
+        Account account = ParameterParser.getSenderAccount(req);
+        Attachment attachment = new Attachment.MessagingAccountInfo(name, description);
+        return createTransaction(req, account, attachment);
+
+    }
+
+}
