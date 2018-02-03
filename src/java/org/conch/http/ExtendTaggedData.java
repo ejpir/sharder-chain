0abbaecc54d7e356c1f23e1da0b40c853c361f98
Aron.j <xy@ichaoj.com>
2018-02-03 21:43:57 +0800

    attachment files

---
+++ src/java/org/conch/http/ExtendTaggedData.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright © 2017 sharder.org.
+ * Copyright © 2014-2017 ichaoj.com.
+ *
+ * See the LICENSE.txt file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with ichaoj.com,
+ * no part of the COS software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE.txt file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+
+package org.conch.http;
+
+import org.conch.Account;
+import org.conch.Attachment;
+import org.conch.Conch;
+import org.conch.ConchException;
+import org.conch.TaggedData;
+import org.conch.Transaction;
+import org.conch.TransactionType;
+import org.json.simple.JSONStreamAware;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.conch.http.JSONResponses.UNKNOWN_TRANSACTION;
+
+public final class ExtendTaggedData extends CreateTransaction {
+
+    static final ExtendTaggedData instance = new ExtendTaggedData();
+
+    private ExtendTaggedData() {
+        super("file", new APITag[] {APITag.DATA, APITag.CREATE_TRANSACTION}, "transaction",
+                "name", "description", "tags", "type", "channel", "isText", "filename", "data");
+    }
+
+    @Override
+    protected JSONStreamAware processRequest(HttpServletRequest req) throws ConchException {
+
+        Account account = ParameterParser.getSenderAccount(req);
+        long transactionId = ParameterParser.getUnsignedLong(req, "transaction", true);
+        TaggedData taggedData = TaggedData.getData(transactionId);
+        if (taggedData == null) {
+            Transaction transaction = Conch.getBlockchain().getTransaction(transactionId);
+            if (transaction == null || transaction.getType() != TransactionType.Data.TAGGED_DATA_UPLOAD) {
+                return UNKNOWN_TRANSACTION;
+            }
+            Attachment.TaggedDataUpload taggedDataUpload = ParameterParser.getTaggedData(req);
+            taggedData = new TaggedData(transaction, taggedDataUpload);
+        }
+        Attachment.TaggedDataExtend taggedDataExtend = new Attachment.TaggedDataExtend(taggedData);
+        return createTransaction(req, account, taggedDataExtend);
+
+    }
+
+}
