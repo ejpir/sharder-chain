dbf1d89ce7f247fe02a4f1d6cd77e54bb45c959d
Aron.j <xy@ichaoj.com>
2018-02-09 16:10:30 +0800

    dump peers

---
+++ src/java/org/conch/http/GetChannelTaggedData.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright © 2017 sharder.org.
+ * Copyright © 2014-2017 ichaoj.com.
+ *
+ * See the LICENSE.txt file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with ichaoj.com,
+ * no part of the COS software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE.txt file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+
+package org.conch.http;
+
+import org.conch.ConchException;
+import org.conch.TaggedData;
+import org.conch.db.DbIterator;
+import org.conch.util.Convert;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONStreamAware;
+
+import javax.servlet.http.HttpServletRequest;
+
+public final class GetChannelTaggedData extends APIServlet.APIRequestHandler {
+
+    static final GetChannelTaggedData instance = new GetChannelTaggedData();
+
+    private GetChannelTaggedData() {
+        super(new APITag[] {APITag.DATA}, "channel", "account", "firstIndex", "lastIndex", "includeData");
+    }
+
+    @Override
+    protected JSONStreamAware processRequest(HttpServletRequest req) throws ConchException {
+        String channel = Convert.emptyToNull(req.getParameter("channel"));
+        if (channel == null) {
+            return JSONResponses.missing("channel");
+        }
+        long accountId = ParameterParser.getAccountId(req, "account", false);
+        int firstIndex = ParameterParser.getFirstIndex(req);
+        int lastIndex = ParameterParser.getLastIndex(req);
+        boolean includeData = "true".equalsIgnoreCase(req.getParameter("includeData"));
+
+        JSONObject response = new JSONObject();
+        JSONArray jsonArray = new JSONArray();
+        response.put("data", jsonArray);
+        try (DbIterator<TaggedData> data = TaggedData.getData(channel, accountId, firstIndex, lastIndex)) {
+            while (data.hasNext()) {
+                jsonArray.add(JSONData.taggedData(data.next(), includeData));
+            }
+        }
+        return response;
+    }
+
+}
